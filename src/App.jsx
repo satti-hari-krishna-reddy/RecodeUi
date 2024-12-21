90ac24a6f80f6def0c8d2e3d45687aec0dbf4205
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2024-12-21 14:49:07 +0530

    improve styling and include about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import Header from './components/Header';
+import About from './components/About';
 
 const App = () => {
     return (
-        <Box
-            sx={{
-                minHeight: '100vh',
-                position: 'relative',
-                overflow: 'hidden',
-                color: 'white',
-                padding: '20px',
-                boxSizing: 'border-box',
-                background: 'linear-gradient(135deg, #ffafbd, #ffc3a0, #d5aaff, #a0c4ff, #8ecae6)',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center center',
-            }}
-        >
-            <Header />
-        </Box>
+<Box
+    sx={{
+        minHeight: '100vh',
+        position: 'relative',
+        overflow: 'hidden',
+        color: 'white',
+        padding: '20px',
+        boxSizing: 'border-box',
+        background: 'linear-gradient(135deg, #ffafbd, #ffc3a0, #d5aaff, #a0c4ff, #8ecae6)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+    }}
+>  <div style={{'display' : 'flex'}}>    <Header />
+<About /></div>
+
+</Box>
+
     );
 };
 
