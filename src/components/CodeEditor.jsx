b8fd52dcfd933504839fb0764581f1f0329fb261
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2024-12-21 23:21:21 +0530

    improving styling of code editor

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -5,11 +5,14 @@ const CodeEditor = () => {
   return (
     <div
       style={{
-        width: '100%',
-        height: '300px', // Fixed height for demo purposes
+        width: '60%',
+        maxHeight: '700px', // Fixed height for demo purposes
+        height: 'fit-content', // Fixed height for demo purposes
         borderRadius: '10px',
         overflow: 'hidden',
         background: '#1E1E1E',
+        padding: '15px',
+        margin: '20px auto',
       }}
     >
       <MonacoEditor
@@ -19,10 +22,11 @@ const CodeEditor = () => {
         theme="vs-dark"
         options={{
           fontSize: 16,
+          
           fontFamily: 'Fira Code, monospace',
           minimap: { enabled: false },
           scrollBeyondLastLine: false,
-          smoothScrolling: true,
+        //   smoothScrolling: true,
           automaticLayout: true,
           wordWrap: 'on',
         }}
